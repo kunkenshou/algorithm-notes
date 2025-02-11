df7d1b00139c6e076fc64e4ba29270bfb4d0093e
kunkenshou <kunkenshou@gmail.com>
2025-02-11 11:27:09 +0900

    Selection-sortのアルゴリズムをロジック、実装を完了

--- algorithm/selection-sort/Selection-sort.vb
+++ algorithm/selection-sort/Selection-sort.vb
@@ -1,46 +1,38 @@
-Sub SelectionSort()
+Sub Selection_sort_0210()
 
+Dim Minimum As Variant
+Dim MinimumIndex As Variant
+Dim X As Variant
 Dim A As Variant
-Dim X As Integer
-Dim Z As Integer
-Dim Lower As Integer
-Dim Upper As Integer
-Dim Mini As Integer
-Dim MiniIndex As Integer
+A = Array(3, 1, 4, 2)
 
-A = Array(2, 1, 3, 4, 6, 5, 9, 8, 10)
-
-Lower = LBound(A)
 Upper = UBound(A)
 
-Mini = A(0)
-MiniIndex = Lower
+For I = 0 To Upper - 1
 
-For I = Lower To Upper - 1
+    'Debug.Print A(I)
 
-    For J = Lower To Upper
-        If Mini > A(J) Then
-            Mini = A(J)
-            MiniIndex = J
-        End If
-    Next J
-    
-    If A(0) > A(I) Then
-         X = A(I)
-         A(MiniIndex) = X
-         A(0) = Mini
+    Minimum = A(I)
+    MinimumIndex = I
+
+For J = I + 1 To Upper
+
+    If A(J) < Minimum Then
+
+        Minimum = A(J)
+        MinimumIndex = J
+
     End If
-    
-    If A(I) > A(I + 1) Then
-         Z = A(I)
-         A(I) = A(I + 1)
-         A(I + 1) = Z
+
+Next J
+
+    If A(I) > Minimum Then
+        X = A(I)
+        A = Minimum
+        'A(MinimumIndex) = X
+
     End If
+
 Next I
 
-'    Debug.Print A(0)
-    For N = Lower To Upper
-        Debug.Print A(N)
-    Next N
-    
 End Sub
