1018c2e07484b91a7a5650c21fc40f78b788c888
kunkenshou <kunkenshou@gmail.com>
2025-02-11 11:25:13 +0900

    最初の要素の交換されない問題の為、変更

--- algorithm/selection-sort/Selection-sort.vb
+++ algorithm/selection-sort/Selection-sort.vb
@@ -1,4 +1,4 @@
-Sub Main()
+Sub SelectionSort()
 
 Dim A As Variant
 Dim X As Integer
@@ -9,31 +9,38 @@ Dim Mini As Integer
 Dim MiniIndex As Integer
 
 A = Array(2, 1, 3, 4, 6, 5, 9, 8, 10)
+
 Lower = LBound(A)
 Upper = UBound(A)
 
-    For I = Lower To Upper - 1
-        Mini = A(0)
-        MiniIndex = Lower
+Mini = A(0)
+MiniIndex = Lower
 
-        If Mini > A(I) Then
-            Mini = A(I)
-            MiniIndex = I
+For I = Lower To Upper - 1
+
+    For J = Lower To Upper
+        If Mini > A(J) Then
+            Mini = A(J)
+            MiniIndex = J
         End If
+    Next J
+    
+    If A(0) > A(I) Then
+         X = A(I)
+         A(MiniIndex) = X
+         A(0) = Mini
+    End If
+    
+    If A(I) > A(I + 1) Then
+         Z = A(I)
+         A(I) = A(I + 1)
+         A(I + 1) = Z
+    End If
+Next I
 
-        X = A(0)
-        A(0) = Mini
-        A(MiniIndex) = X
-
-        If A(I) > A(I + 1) Then
-             Z = A(I + 1)
-             A(I + 1) = A(I)
-             A(I) = Z
-        End If
-    Next I
-
+'    Debug.Print A(0)
     For N = Lower To Upper
         Debug.Print A(N)
     Next N
-
+    
 End Sub
